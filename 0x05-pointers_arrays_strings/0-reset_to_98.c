a03faa3224667bfb4ee50b14cbb686e1ff819f2f
AlvinMunai <alvin.munai@gmail.com>
2022-09-19 17:36:47 -0700

    Task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Updates n to 98
+ * @n: pointer to int
+ *
+ * Return: void, meaning it is correct
+ */
+
+void reset_to_98(int *n)
+
+{
+	*n = 98;
+}
+
+
