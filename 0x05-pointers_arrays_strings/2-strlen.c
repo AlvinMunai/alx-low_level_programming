bf0a0a88dc786413f966d0e9cafc2dcbbc1534e5
AlvinMunai <alvin.munai@gmail.com>
2022-09-19 18:01:08 -0700

    Task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strlen - length of string
+ * @s: Pointer to string
+ *
+ * Return: Void (Meaning we are correct)
+ */
+
+int _strlen(char *s)
+{
+	int t;
+
+	t = 0;
+
+	while (s[t] != '\0')
+	{
+		t++
+	}
+
+	return (t);
+}
